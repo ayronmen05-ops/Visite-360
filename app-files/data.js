0f01fd642e40ebda6e9105f8926f6421c5db2178
ayronmen05-ops <ayronmen05@gmail.com>
2025-12-12 01:40:53 +0100

    Initial commit - visite virtuelle Marzipano

---
+++ app-files/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1516.5,
+      "initialViewParameters": {
+        "yaw": -1.799105137591198,
+        "pitch": 0.10565576670728305,
+        "fov": 1.2721933609937348
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.132206186249438,
+          "pitch": 0.16124367785286609,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.3830078141366151,
+          "pitch": 0.21259492649087974,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1596.5,
+      "initialViewParameters": {
+        "yaw": 2.0119287491523608,
+        "pitch": 0.02297299436027167,
+        "fov": 1.2721933609937348
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5408366827573943,
+          "pitch": 0.1752905815024679,
+          "rotation": 13.351768777756625,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1518.5,
+      "initialViewParameters": {
+        "yaw": -2.751314773536871,
+        "pitch": -0.03571918509807048,
+        "fov": 1.2721933609937348
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1415559307835945,
+          "pitch": -0.14165979366601178,
+          "rotation": 0.7853981633974483,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
